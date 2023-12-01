cf61f284b9a73cf305b707a0c212c8c35c6e1828
KaterynaKateryna <tok.ar.tok@gmail.com>
2023-12-01 08:54:38 +0100

    day 1 part 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
@@ -9,7 +11,20 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        string[] lines = _input.Split(Environment.NewLine);
+        long sum = 0;
+
+        foreach (string item in lines)
+        {
+            MatchCollection matches = Regex.Matches(item, "[0-9]");
+            int calibrationValue = int.Parse(matches.First().Value + matches.Last().Value);
+            sum += calibrationValue;
+        }
+
+        return new(sum.ToString());
+    }
 
     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
 }
