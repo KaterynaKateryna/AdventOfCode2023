fae999fbd02d1ce77489cfd65c85f48e851c30d5
KaterynaKateryna <tok.ar.tok@gmail.com>
2023-12-02 19:49:07 +0100

    day 2 part 1

---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,70 @@
+﻿namespace AdventOfCode;
+
+public class Day02 : BaseDay
+{
+    private readonly Game[] _input;
+
+    public Day02()
+    {
+        _input = File.ReadAllLines(InputFilePath).Select(l => Game.Parse(l)).ToArray();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        int redLimit = 12;
+        int greenLimit = 13;
+        int blueLimit = 15;
+
+        int result = 0;
+        foreach (Game game in _input)
+        {
+            if (game.Rounds.All(r => r.Red <= redLimit && r.Green <= greenLimit && r.Blue <= blueLimit))
+            {
+                result += game.Index;
+            }
+        }
+        return new(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("TBD");
+    }
+
+    private record Game(int Index, Round[] Rounds)
+    {
+        public static Game Parse(string input)
+        {
+            string[] parts = input.Split(":");
+            int index = int.Parse(parts[0].Split(" ")[1]);
+            Round[] rounds = parts[1].Split(";").Select(roundInput => Round.Parse(roundInput.Split(","))).ToArray();
+            return new Game(index, rounds);
+        }
+    }
+
+    private record Round(int Red, int Green, int Blue)
+    {
+        public static Round Parse(string[] round)
+        {
+            int red = 0, green = 0, blue = 0;
+            foreach (string input in round)
+            {
+                string[] parts = input.Trim().Split(" ");
+                int count = int.Parse(parts[0]);
+                switch (parts[1])
+                {
+                    case "red":
+                        red = count;
+                        break;
+                    case "green":
+                        green = count;
+                        break;
+                    case "blue":
+                        blue = count;
+                        break;
+                }
+            }
+            return new Round(red, green, blue);
+        }
+    }
+}
