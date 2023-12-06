9076c53ca3b94c6a9e8cc4da06e485da5e534e3f
KaterynaKateryna <tok.ar.tok@gmail.com>
2023-12-06 10:58:14 +0100

    day 6 part 2

--- AdventOfCode/Day06.cs
+++ AdventOfCode/Day06.cs
@@ -2,42 +2,46 @@
 
 public class Day06 : BaseDay
 {
-    private Race[] _races;
+    private string[] _lines;
 
     public Day06()
     {
-        string[] lines = File.ReadAllLines(InputFilePath);
-        string[] times = lines[0].Replace("Time:", "").Split(" ", StringSplitOptions.RemoveEmptyEntries);
-        string[] distances = lines[1].Replace("Distance:", "").Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-        _races = new Race[times.Length];
-        for (int i = 0; i < times.Length; ++i)
-        {
-            _races[i] = new Race(int.Parse(times[i]), int.Parse(distances[i]));
-        }
+        _lines = File.ReadAllLines(InputFilePath);
     }
 
     public override ValueTask<string> Solve_1()
     {
-        int result = _races.Aggregate(1, (result, race) => result * GetWaysToWin(race));
+        string[] times = _lines[0].Replace("Time:", "").Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        string[] distances = _lines[1].Replace("Distance:", "").Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        Race[] races = new Race[times.Length];
+        for (int i = 0; i < times.Length; ++i)
+        {
+            races[i] = new Race(int.Parse(times[i]), int.Parse(distances[i]));
+        }
+
+        long result = races.Aggregate(1L, (result, race) => result * GetWaysToWin(race));
         return new(result.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        return new("TBD");
+        long time = long.Parse(_lines[0].Replace("Time:", "").Replace(" ", ""));
+        long distance = long.Parse(_lines[1].Replace("Distance:", "").Replace(" ", ""));
+
+        return new(GetWaysToWin(new Race(time, distance)).ToString());
     }
 
-    private int GetWaysToWin(Race race)
+    private long GetWaysToWin(Race race)
     {
-        int waysToWin = 0;
-        for (int i = 0; i < race.Time; ++i)
+        long waysToWin = 0;
+        for (long i = 0; i < race.Time; ++i)
         {
-            int distance = (race.Time - i) * i;
+            long distance = (race.Time - i) * i;
             waysToWin += distance > race.Distance ? 1 : 0;
         }
         return waysToWin;
     }
 
-    private record Race(int Time, int Distance);
+    private record Race(long Time, long Distance);
 }
