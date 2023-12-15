ede0ba19f198f7052cab80dde09f1894cdc3e53b
KaterynaKateryna <tok.ar.tok@gmail.com>
2023-12-15 19:58:02 +0100

    day 15 part 1

---
+++ AdventOfCode/Day15.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode;
+
+public class Day15 : BaseDay
+{
+    private string _input;
+
+    public Day15()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        string[] steps = _input.Split(',', StringSplitOptions.RemoveEmptyEntries);
+        int sum = steps.Sum(GetHashCode);
+
+        return new(sum.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("TBD");
+    }
+
+    private int GetHashCode(string step)
+    {
+        int result = 0;
+        foreach(char ch in step) 
+        {
+            result += ch;
+            result *= 17;
+            result %= 256;
+        }
+
+        return result;
+    }
+}
