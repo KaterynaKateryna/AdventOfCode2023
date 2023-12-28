586ee236ceeade3d33aa878833c869f9f4654db0
KaterynaKateryna <tok.ar.tok@gmail.com>
2023-12-28 23:06:03 +0100

    day 14 part 2

--- AdventOfCode/Day14.cs
+++ AdventOfCode/Day14.cs
@@ -4,9 +4,41 @@ public class Day14 : BaseDay
 {
     private char[][] _input;
 
-    public Day14() 
+    private HashSet<Point> _roundRocks = new();
+    private HashSet<Point> _squareRocks = new();
+
+    private HashSet<Point> _rocks = new();
+    private Dictionary<int, List<Point>> _roundRocksByRow = new();
+
+    public Day14()
     {
         _input = File.ReadAllLines(InputFilePath).Select(l => l.ToArray()).ToArray();
+
+        for (int i = 0; i < _input.Length; ++i)
+        {
+            for (int j = 0; j < _input[i].Length; ++j)
+            {
+                if (_input[i][j] == 'O')
+                {
+                    _roundRocks.Add(new Point(i, j));
+                    _rocks.Add(new Point(i, j));
+                }
+                if (_input[i][j] == '#')
+                {
+                    _squareRocks.Add(new Point(i, j));
+                    _rocks.Add(new Point(i, j));
+                }
+            }
+        }
+
+        _roundRocksByRow = _roundRocks.GroupBy(k => k.I).ToDictionary(k => k.Key, k => k.ToList());
+        for (int i = 0; i < _input.Length; ++i)
+        {
+            if (!_roundRocksByRow.ContainsKey(i))
+            {
+                _roundRocksByRow[i] = new List<Point>();
+            }
+        }
     }
 
     public override ValueTask<string> Solve_1()
@@ -18,36 +50,130 @@ public class Day14 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        return new("TBD");
+        HashSet<string> maps = new HashSet<string>();
+
+        string key = "";
+        do
+        {
+            Cycle();
+            key = string.Join(Environment.NewLine, _input.Select(l => new string(l)));
+        }
+        while (maps.Add(key));
+
+
+        int rest = 1_000_000_000 % maps.Count;
+
+        for (int i = 0; i < rest - 1; ++i)
+        {
+            Cycle();
+        }
+
+        int load = GetLoad();
+        return new(load.ToString());
+    }
+
+    private void Cycle()
+    {
+        TiltNorth();
+        TiltWest();
+        TiltSouth();
+        TiltEast();
     }
 
     private void TiltNorth()
     {
         for (int i = 1; i < _input.Length; ++i)
         {
-            for (int j = 0; j < _input[i].Length; ++j)
+            for (int j = 0; j < _input.Length; ++j)
             {
                 if (_input[i][j] == 'O')
                 {
-                    RollNorth(i, j);
+                    int replace = i - 1;
+                    while (replace >= 0 && _input[replace][j] == '.')
+                    {
+                        replace--;
+                    }
+                    replace++;
+                    if (replace != i)
+                    {
+                        _input[i][j] = '.';
+                        _input[replace][j] = 'O';
+                    }
                 }
             }
         }
     }
 
-    private void RollNorth(int i, int j)
+    private void TiltWest()
     {
-        int replace = i-1;
-        while (replace >= 0 && _input[replace][j] == '.')
+        for (int i = 0; i < _input.Length; ++i)
         {
-            replace--;
+            for (int j = 1; j < _input[i].Length; ++j)
+            {
+                if (_input[i][j] == 'O')
+                {
+                    int replace = j - 1;
+                    while (replace >= 0 && _input[i][replace] == '.')
+                    {
+                        replace--;
+                    }
+                    replace++;
+                    if (replace != j)
+                    {
+                        _input[i][j] = '.';
+                        _input[i][replace] = 'O';
+                    }
+                }
+            }
         }
-        replace++;
+    }
 
-        if (replace != i)
+    private void TiltSouth()
+    {
+        for (int i = _input.Length - 2; i >= 0; --i)
         {
-            _input[i][j] = '.';
-            _input[replace][j] = 'O';
+            for (int j = 0; j < _input[0].Length; ++j)
+            {
+                if (_input[i][j] == 'O')
+                {
+                    int replace = i + 1;
+                    while (replace < _input.Length && _input[replace][j] == '.')
+                    {
+                        replace++;
+                    }
+                    replace--;
+                    if (replace != i)
+                    {
+                        _input[i][j] = '.';
+                        _input[replace][j] = 'O';
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void TiltEast()
+    {
+        for (int i = 0; i < _input.Length; ++i)
+        {
+            for (int j = _input[i].Length - 2; j >= 0; --j)
+            {
+                if (_input[i][j] == 'O')
+                {
+                    int replace = j + 1;
+                    while (replace < _input.Length && _input[i][replace] == '.')
+                    {
+                        replace++;
+                    }
+                    replace--;
+                    if (replace != j)
+                    {
+                        _input[i][j] = '.';
+                        _input[i][replace] = 'O';
+                    }
+                }
+            }
         }
     }
 
@@ -59,5 +185,12 @@ public class Day14 : BaseDay
             load += _input[i].Count(x => x == 'O') * (_input.Length - i);
         }
         return load;
+    }
+
+    private record Point(int I, int J)
+    {
+        public int I { get; set; } = I;
+
+        public int J { get; set; } = J;
     }
 }
